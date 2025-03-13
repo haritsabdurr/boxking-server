c7e184287f7addff5feb3afe20fbcbcdb86c84cb
haritsabdurr <abdurrahman.825190102@stu.untar.ac.id>
2025-03-13 13:33:43 +0700

    fixed relative path on models

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
-const db = require('../server/models');
+const db = require('./models');
 db.mongoose
   .connect(db.url, {
     useNewUrlParser: true,
